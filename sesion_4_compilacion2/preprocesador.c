f68f5c2f9d595beb98612e385437f7b8f35c5091
dac-uma <felipe@uma.es>
2023-10-04 11:34:38 +0200

    Subidos varios pdfs

--- sesion_4_compilacion2/preprocesador.c
+++ sesion_4_compilacion2/preprocesador.c
@@ -1,13 +1,17 @@
 #include <math.h>
 #define PEPE for
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+#define NUM 23+4
+
 
 float *a;
 float *b;
 float *v;
 int main()
 {
-	PEPE(int i=0;i<1000;i++)
-	{
-		v[i]=a[i]+b[i];
-	}
+        PEPE(int i=0;i<min(NUM,1000);i++)
+        {
+                v[i]=a[i]+b[i];
+        }
 }
