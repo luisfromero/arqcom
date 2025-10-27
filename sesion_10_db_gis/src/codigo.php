322b868c854f1c5e77bde14853d9ed3053907bb2
luisfromero <felipe@uma.es>
2025-10-27 08:08:08 +0100

    Bases de datos, gis

---
+++ sesion_10_db_gis/src/codigo.php
@@ -0,0 +1,8 @@
+Hola esto es html normal --
+<?php
+
+$a=3;
+$b= 3;
+$c= $a+$b;
+echo $a.' + '.$b.' = '.$c.PHP_EOL;
+?>
--- sesion_5_lazosSaltos/loop0.s
+++ sesion_5_lazosSaltos/loop0.s
@@ -1,53 +1,41 @@
 	.file	"loop0.c"
 	.option pic
 	.text
+	.align	1
+	.globl	function
+	.type	function, @function
+function:
+	lla	a5,c
+	lla	a4,c+8192
+	fld	fa5,.LC0,a3
+.L2:
+	fld	fa1,0(a5)
+	fld	fa2,8(a5)
+	fld	fa3,16(a5)
+	fld	fa4,24(a5)
+	fmul.d	ft0,fa1,fa5
+	fmul.d	ft1,fa2,fa5
+	fmul.d	ft2,fa3,fa5
+	fmul.d	ft3,fa4,fa5
+	addi	a5,a5,32
+	fsd	ft0,-32(a5)
+	fsd	ft1,-24(a5)
+	fsd	ft2,-16(a5)
+	fsd	ft3,-8(a5)
+	bne	a5,a4,.L2
+	ret
+	.size	function, .-function
 	.globl	c
+	.section	.rodata.cst8,"aM",@progbits,8
+	.align	3
+.LC0:
+	.word	0
+	.word	1071644672
 	.bss
 	.align	3
 	.type	c, @object
 	.size	c, 8192
 c:
 	.zero	8192
-	.text
-	.align	1
-	.globl	function
-	.type	function, @function
-function:
-	addi	sp,sp,-16
-	sd	s0,8(sp)
-	sd	s1,0(sp)
-	addi	s0,sp,16
-	li	s1,0
-	j	.L2
-.L3:
-	lla	a4,c
-	slli	a5,s1,3
-	add	a5,a4,a5
-	fld	fa4,0(a5)
-	lla	a5,.LC0
-	fld	fa5,0(a5)
-	fdiv.d	fa5,fa4,fa5
-	lla	a4,c
-	slli	a5,s1,3
-	add	a5,a4,a5
-	fsd	fa5,0(a5)
-	addiw	a5,s1,1
-	sext.w	s1,a5
-.L2:
-	mv	a4,s1
-	li	a5,1023
-	ble	a4,a5,.L3
-	nop
-	nop
-	ld	s0,8(sp)
-	ld	s1,0(sp)
-	addi	sp,sp,16
-	jr	ra
-	.size	function, .-function
-	.section	.rodata
-	.align	3
-.LC0:
-	.word	0
-	.word	1073741824
 	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
 	.section	.note.GNU-stack,"",@progbits
